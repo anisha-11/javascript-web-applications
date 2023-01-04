d22f28baa45b5e4553e17c048b4c294e26683d1b
Anisha Hirani <anisha.hirani11@gmail.com>
2023-01-04 15:32:50 +0000

    customising message complete

--- phase-2/example-3/messageView.test.js
+++ phase-2/example-3/messageView.test.js
@@ -6,15 +6,21 @@ const fs = require('fs');
 const MessageView = require('./messageView');
 
 describe('MessageView', () => {
-  it('clicks the button', () => {
+  it('clicks the button and displays that message', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
 
     const view = new MessageView();
 
     const buttonEl = document.querySelector('#show-message-button');
+    const inputEl = document.querySelector('#message-input');
+
+    inputEl.value = 'This is a test message';
+
     buttonEl.click();
 
     expect(document.querySelector('#message')).not.toBeNull();
+    expect(document.querySelector('#message').innerText).toEqual('This is a test message');
+
   });
 
    it('hides the message', () => {
